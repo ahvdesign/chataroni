529b5d6b8266c1d52318fd2ee08481a8ef56f2c4
Alexander Henriquez <ahvdesign@gmail.com>
2023-06-13 15:06:58 -0400

    Added theme selector

---
+++ src/lib/ThemeSelect.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  enum Theme {
+    light = "light",
+    dark = "dark",
+    cupcake = "cupcake",
+    bumblebee = "bumblebee",
+    emerald = "emerald",
+    corporate = "corporate",
+    synthwave = "synthwave",
+    retro = "retro",
+    cyberpunk = "cyberpunk",
+    valentine = "valentine",
+    halloween = "halloween",
+    garden = "garden",
+    forest = "forest",
+    aqua = "aqua",
+    lofi = "lofi",
+    pastel = "pastel",
+    fantasy = "fantasy",
+    wireframe = "wireframe",
+    black = "black",
+    luxury = "luxury",
+    dracula = "dracula",
+    cmyk = "cmyk",
+    autumn = "autumn",
+    business = "business",
+    acid = "acid",
+    lemonade = "lemonade",
+    night = "night",
+    coffee = "coffee",
+    winter = "winter",
+  }
+  const themes: Theme[] = Object.values(Theme);
+
+  function changeTheme() {
+    const theme = (event.target as HTMLSelectElement).value;
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+</script>
+
+<select on:change={changeTheme} class="select select-ghost w-auto max-w-xs">
+  <option disabled selected>Theme</option>
+  {#each themes as theme}
+    <option value={theme}>{theme}</option>
+  {/each}
+</select>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './app.css'
+import "./input.css"
 import App from './App.svelte'
 
 const app = new App({
